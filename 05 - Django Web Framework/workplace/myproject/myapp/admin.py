45a99f95b9c6b3a4974a6e313313d597a4192e5d
Leandro Nicolás Borquez Zuloaga <borquezln@hotmail.com>
2023-08-24 21:12:29 -0300

    Complete course Django Web Framework

--- 05 - Django Web Framework/workplace/myproject/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Booking, Drinks, DrinksCategory, Employees
-
-# Register your models here.
-admin.site.register(Booking)
-admin.site.register(Drinks)
-admin.site.register(DrinksCategory)
-admin.site.register(Employees)
--- 05 - Django Web Framework/workplace/myproject/myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Booking
-
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model = Booking
-        fields = "__all__"
